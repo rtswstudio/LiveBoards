175daa4a75210e1ae0dd90b654b7215439b71420
RT Software Studio <rt.sw.studio@gmail.com>
2024-03-14 23:50:07 +0200

    Support timestamps for min/max aggregation

--- src/com/rtsw/liveboards/board/aggregation/MaximumAggregation.java
+++ src/com/rtsw/liveboards/board/aggregation/MaximumAggregation.java
@@ -1,12 +1,13 @@
 package com.rtsw.liveboards.board.aggregation;
 
+import java.util.Date;
 import java.util.Set;
 
 public class MaximumAggregation implements Aggregation {
 
     private Object value;
 
-    public static Set<Class> ALLOWED_CLASSES = Set.of(Integer.class, Double.class);
+    public static Set<Class> ALLOWED_CLASSES = Set.of(Integer.class, Double.class, Date.class);
 
     public MaximumAggregation(Object value) {
         this.value = value;
@@ -25,6 +26,13 @@ public class MaximumAggregation implements Aggregation {
             Integer a = (Integer) value;
             Integer b = (Integer) another;
             if (b > a) {
+                value = b;
+            }
+        }
+        if (value instanceof Date && another instanceof Date) {
+            Date a = (Date) value;
+            Date b = (Date) another;
+            if (b.after(a)) {
                 value = b;
             }
         }
